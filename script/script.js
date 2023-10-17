cf9fb1fc270ab1ca1c7cb5b259ebfbf58aaab0fd
Fernando <fer3443@gmail.com>
2023-10-17 18:16:54 -0300

    animaciones agregadas con scrollReveal

---
+++ script/script.js
@@ -0,0 +1,22 @@
+if(document.title == "Home"){
+    ScrollReveal().reveal('.boxTitle', {
+        delay: 500,
+        opacity: 0,
+        distance: '-50px'
+    });
+    ScrollReveal().reveal('#card1', {
+        delay: 400,
+        opacity: 0,
+        distance:'20px'
+    })
+    ScrollReveal().reveal('#card2', {
+        delay: 600,
+        opacity: 0,
+        distance:'20px'
+    })
+    ScrollReveal().reveal('#card3', {
+        delay: 800,
+        opacity: 0,
+        distance:'20px'
+    })
+}
