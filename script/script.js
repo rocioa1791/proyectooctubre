fe34631c89db4dfedee63348f829a8aa5d2e07a3
Fernando <fer3443@gmail.com>
2023-10-18 14:21:04 -0300

    tarjetas en characters creada y llamado a la api funcional

--- script/script.js
+++ script/script.js
@@ -1,22 +1,37 @@
-if(document.title == "Home"){
-    ScrollReveal().reveal('.boxTitle', {
-        delay: 500,
-        opacity: 0,
-        distance: '-50px'
-    });
-    ScrollReveal().reveal('#card1', {
-        delay: 400,
-        opacity: 0,
-        distance:'20px'
+if (document.title == "Home") {
+  ScrollReveal().reveal(".boxTitle", {
+    delay: 500,
+    opacity: 0,
+    distance: "-50px",
+  });
+  ScrollReveal().reveal("#card1", {
+    delay: 400,
+    opacity: 0,
+    distance: "20px",
+  });
+  ScrollReveal().reveal("#card2", {
+    delay: 600,
+    opacity: 0,
+    distance: "20px",
+  });
+  ScrollReveal().reveal("#card3", {
+    delay: 800,
+    opacity: 0,
+    distance: "20px",
+  });
+}
+if (document.title == "Personajes") {
+  const urlCharacters = "https://api.sampleapis.com/futurama/characters";
+  const characters = [];
+  fetch(urlCharacters)
+    .then((res) => {
+      const response = res.json();
+      return response;
     })
-    ScrollReveal().reveal('#card2', {
-        delay: 600,
-        opacity: 0,
-        distance:'20px'
+    .then((data) => {
+      characters.push(data);
     })
-    ScrollReveal().reveal('#card3', {
-        delay: 800,
-        opacity: 0,
-        distance:'20px'
-    })
+    .catch((err) => console.log(err));
+  
+  console.log(characters)
 }
